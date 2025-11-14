061f158d11e0015d5247f661549a86d299e72bfd
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-13 23:00:30 -0600

    WIP: Added seed for creating a single user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(email: "chad_bro_chill@notfakemail.com", password: "lowkeybussin", password_confirmation: "lowkeybussin")
+
