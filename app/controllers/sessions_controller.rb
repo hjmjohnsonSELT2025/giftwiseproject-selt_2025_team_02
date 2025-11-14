0fd1cfdbbd8b3c55c8a361741f215d67dd6239af
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-13 22:40:08 -0600

    WIP: Replaced deprecated skip_before_filter with skip_before_action, resets token on login, destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,22 @@
 class SessionsController < ApplicationController
-  skip_before_filter :set_current_user
+  skip_before_action :set_current_user
 
   def new
   end
 
   def create
-    user = User.find_by_email(params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      #log in and redirect to show page
-      session[:session_token] = user.session_token
-      redirect_to homepage_path
+    user = User.find_by(email: params.dig(:session, :email))
+    if user&.authenticate(params.dig(:session, :password))
+      session[:session_token] = user.reset_session_token!
+      redirect_to homepage_path, notice: "Logged in!"
     else
-      flash.now[:warning] = 'Incorrect email and/or password'
-      render 'new'
+      flash.now[:warning] = "Incorrect email and/or password"
+      render :new, status: :unprocessable_entity
     end
+  end
+  def destroy
+    current_user&.reset_session_token!
+    session.delete(:session_token)
+    redirect_to sign_in_path, notice: "Signed out"
   end
 end
