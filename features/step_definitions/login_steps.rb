0c427b0577aa8c2246cf0b0f254044b90ed00cdb
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-13 22:06:51 -0600

    WIP: Filled in user exists and go to login page step definitions

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,7 +1,9 @@
 Given("a user exists with email {string} and password {string}") do |email, password|
+  @user = User.create!(email: email, password: password, password_confirmation: password)
 end
 
 When("I go to the log in page") do
+  visit "/login"
 end
 
 Then("I should be on the log in page") do
