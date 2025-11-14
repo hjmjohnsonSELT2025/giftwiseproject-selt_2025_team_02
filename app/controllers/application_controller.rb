36ecbc0980e601692b01af646ce1e8e93d0b43da
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-13 22:03:55 -0600

    WIP: Framing out ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  protect_from_forgery with: :exception
+  protected
+  def set_current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+    redirect_to_login_path unless @current_user
+  end
 end
