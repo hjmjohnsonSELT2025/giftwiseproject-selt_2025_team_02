fe1845e29ecc6063713682054d8318c502d2908f
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-13 22:53:34 -0600

    WIP: Created current user helper methods in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,25 @@ class ApplicationController < ActionController::Base
   stale_when_importmap_changes
 
   protect_from_forgery with: :exception
+
+  before_action :set_current_user
+  helper_method :current_user, :current_user?, :logged_in?
+
   protected
   def set_current_user
     @current_user ||= User.find_by_session_token(session[:session_token])
     redirect_to_login_path unless @current_user
   end
 
+  def current_user
+    @current_user
+  end
   def current_user?(id)
     @current_user.id.to_s == id
   end
+
+  def logged_in?
+    current_user.present?
+  end
+
 end
