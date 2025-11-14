2419398716ffd68ee660454d25c081487790d9ad
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-13 21:25:45 -0600

    WIP: Framed out login_steps.rb in step_definitions

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,20 @@
+Given("a user exists with email {string} and password {string}") do |email, password|
+end
+
+When("I go to the log in page") do
+end
+
+Then("I should be on the log in page") do
+end
+
+Then("I should be on the home page") do
+end
+
+Then("I should be logged in") do
+end
+
+Then("I should not be logged in") do
+end
+
+Then('I should see {string} within the flash') do |words|
+end
