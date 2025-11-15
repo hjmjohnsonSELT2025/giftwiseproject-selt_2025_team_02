6fbc16124364b4b6e9eabd39d2e969ed9caccf5a
Jabe03 <Bergthold.joshua03@gmail.com>
2025-11-14 23:27:40 -0600

    Resolved model structure, added link to gifts to homepage (TEMPORARY)

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,7 @@
 class Gift < ApplicationRecord
   validates :name, presence: true
-  belongs_to :user
+  belongs_to :gift_list
+  has_one :recipient, through: :gift_list
+  has_one :user, through: :recipient
 end
+
