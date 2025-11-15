a09860b12575fffb5b8f904c0bd15e9f127eeacd
Nancy Nahra <nnahra@uiowa.edu>
2025-11-14 19:01:40 -0600

    Gifts controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -4,7 +4,24 @@ class GiftsController < ApplicationController
   end
 
   def show
-
     @gift = Gift.find(params[:id])
+  end
+
+  def new
+    @gift = Gift.new
+  end
+
+  def create
+    @gift = Gift.new(gift_params)
+    if @gift.save
+      redirect_to @gift
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def gift_params
+    params.expect(gift: [ :name])
   end
 end
