dec1446cb2cabaed3954e80f9c96cc3277fdf0d7
Ella Potter <egpotter@uiowa.edu>
2025-11-21 17:00:07 -0600

    feature(Users): Add in profile show and edit routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,12 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+  validates :password_confirmation, presence: true, if: :password_required?
+
+  def password_required?
+    password_digest.blank? || password.present?
+  end
 
   def reset_session_token!
     new_token = create_session_token
