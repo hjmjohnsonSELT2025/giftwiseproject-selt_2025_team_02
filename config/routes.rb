a683b717b7e744fff65a9c3d00647683893e9af4
Ella Potter <egpotter@uiowa.edu>
2025-11-23 13:07:38 -0600

    bugfix(routes.rb): Fix merge issues with main branch, trying again

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,13 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :sessions, only: [ :new, :create, :destroy ]
-  resources :users, only: [ :new, :create, :destroy ]
-
   resources :recipients do
     resources :gift_lists do
       resources :gifts
     end
   end
+
+  resources :users, only: [ :new, :create, :destroy ]
 
   get    "/login",  to: "sessions#new",     as: :login
   post   "/login",  to: "sessions#create"
