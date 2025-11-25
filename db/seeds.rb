fe9cf9ee50b3421250e70e12324434495f5e02b6
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-25 09:21:35 -0600

    feature(seeds.rb): Updated seeds.rb for event, user, recipient testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,37 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.find_or_create_by!(email: "chad_bro_chill@fakemail.com") do |u|
+user = User.find_or_create_by!(email: "chad_bro_chill@fakemail.com") do |u|
   u.name  = "Chad"
   u.password = "lowkeybussin"
   u.password_confirmation = "lowkeybussin"
 end
+
+recipient1 = user.recipients.find_or_create_by!(name: "Thad") do |r|
+  r.age       = 20
+  r.gender    = :male
+  r.relation  = "Bro"
+  r.likes     = %w[Yoga Meditation]
+  r.dislikes  = %w[Math]
+end
+
+recipient2 = user.recipients.find_or_create_by!(name: "Brad") do |r|
+  r.age       = 21
+  r.gender    = :male
+  r.relation  = "Bro"
+  r.likes     = %w[Reading Technology Theater]
+  r.dislikes  = %w[Bright Lights]
+end
+
+event = user.events.find_or_create_by!(name: "Beer oclock") do |e|
+  e.event_date = Date.today + 4
+  e.event_time = Time.zone.parse("22:00")
+  e.location   = "Downtown"
+  e.budget     = 50
+end
+
+
+[ recipient1, recipient2 ].each do |rec|
+  event.recipients << rec unless event.recipients.exists?(rec.id)
+end
+
