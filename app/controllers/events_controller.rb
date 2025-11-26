11e35e30e6dec10e2b286be653a9926c44bf6414
Brandon Rea <brandon-rea@uiowa.edu>
2025-11-26 12:23:32 -0600

    feature(events_controller.rb): Add add_recipient, remove_recipient to EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,12 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[show edit update destroy]
+  before_action :set_event, only: %i[show edit update destroy add_recipient remove_recipient]
 
   def index
     @events = current_user.events.order(event_date: :asc, event_time: :asc)
   end
 
   def show
+    @available_recipients = current_user.recipients.where.not(id: @event.recipient_ids)
   end
 
   def new
@@ -37,6 +38,20 @@ class EventsController < ApplicationController
     name = @event.name
     @event.destroy
     redirect_to events_path, notice: "Event '#{name}' deleted."
+  end
+
+  def add_recipient
+    recipient = current_user.recipients.find(params[:recipient_id])
+    @event.recipients << recipient unless @event.recipients.exists?(recipient.id)
+    redirect_to event_path(@event),
+                notice: "Recipient '#{recipient.name}' successfully added to '#{@event.name}'."
+  end
+
+  def remove_recipient
+    recipient = @event.recipients.find(params[:recipient_id])
+    @event.recipients.delete(recipient)
+    redirect_to event_path(@event),
+                notice: "Recipient '#{recipient.name}' successfully removed from '#{@event.name}'."
   end
 
   private
