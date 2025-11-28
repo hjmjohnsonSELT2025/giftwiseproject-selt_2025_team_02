726de1e14732b665afbabee8070047587fa5e076
Ella Potter <egpotter@uiowa.edu>
2025-11-28 10:06:02 -0600

    feature: Add in budget controller

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :event_recipient_budgets
+
   get    "/login",  to: "sessions#new",     as: :login
   post   "/login",  to: "sessions#create"
   delete "/logout", to: "sessions#destroy", as: :logout
