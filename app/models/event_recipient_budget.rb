6024af10bf79b3927a9c4c811548f94a74aee567
Ella Potter <egpotter@uiowa.edu>
2025-11-28 10:01:05 -0600

    feature(models): add in db relations for event recipients and budget

--- app/models/event_recipient_budget.rb
+++ app/models/event_recipient_budget.rb
@@ -1,3 +1,8 @@
 class EventRecipientBudget < ApplicationRecord
   belongs_to :event_recipient
+  has_one :event, through: :event_recipient
+  has_one :recipient, through: :event_recipient
+
+  validates :budget, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
+  validates :spent, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
 end
