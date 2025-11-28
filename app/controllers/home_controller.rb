bd4b2588a4d3156569bc5b0e911e8e1a8884f1df
Jabe03 <jbergthold@uiowa.edu>
2025-11-28 16:40:18 -0600

    Turbo tabs implemented, gift lists WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def show
+    @tab = params[:tab] || "overview"
     @recipients = current_user.recipients.order(:name)
     @events = current_user.events.order(:event_date, :event_time)
   end
