b95a27e276673de3f2ce43b8b1b058c22a4aa72a
Jabe03 <Bergthold.joshua03@gmail.com>
2025-11-28 23:26:17 -0600

    fixed security concern

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
+  TABS = {
+    "overview" => "home/tabs/overview",
+    "events" => "home/tabs/events",
+    "recipients" => "home/tabs/recipients",
+    "budgets" => "home/tabs/budgets",
+    "lists" => "home/tabs/lists"
+
+  }.freeze
+
+
   def show
-    @tab = params[:tab] || "overview"
+    @tab = TABS[params[:tab]] || TABS["overview"]
     @recipients = current_user.recipients.order(:name)
     @events = current_user.events.order(:event_date, :event_time)
     @budgets = EventRecipientBudget
@@ -9,4 +19,8 @@ class HomeController < ApplicationController
       .includes(:event_recipient, :event, :recipient)
       .order("events.event_date")
   end
+
+
+
+
 end
