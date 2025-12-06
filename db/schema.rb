9e1064cb7cf3725a4c0aec0c2a99d91a1eed2437
Nancy Nahra <nnahra@uiowa.edu>
2025-12-06 13:55:08 -0600

    When destroying a recipient, ensure that the associations (gift lists and gifts) are also destroyed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_28_154708) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_05_043633) do
   create_table "event_recipient_budgets", force: :cascade do |t|
     t.decimal "budget", precision: 10, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
@@ -44,10 +44,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_154708) do
   end
 
   create_table "gift_lists", force: :cascade do |t|
+    t.boolean "archived", default: false, null: false
     t.datetime "created_at", null: false
+    t.integer "event_id"
     t.string "name"
     t.integer "recipient_id", null: false
+    t.string "title"
     t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_gift_lists_on_event_id"
     t.index ["recipient_id"], name: "index_gift_lists_on_recipient_id"
   end
 
@@ -68,8 +72,10 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_154708) do
     t.datetime "created_at", null: false
     t.integer "gift_list_id"
     t.string "name"
+    t.integer "status", default: 0
     t.datetime "updated_at", null: false
     t.index ["gift_list_id"], name: "index_gifts_on_gift_list_id"
+    t.index ["status"], name: "index_gifts_on_status"
   end
 
   create_table "recipients", force: :cascade do |t|
@@ -112,6 +118,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_154708) do
   add_foreign_key "event_recipients", "events"
   add_foreign_key "event_recipients", "recipients"
   add_foreign_key "events", "users"
+  add_foreign_key "gift_lists", "events"
   add_foreign_key "gift_lists", "recipients"
   add_foreign_key "gift_offers", "gifts"
   add_foreign_key "gifts", "gift_lists"
