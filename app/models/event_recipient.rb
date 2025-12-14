de85739fc1314a61fd29f314aa3194cfb83b8d3a
Joshua Bergthold <jbergthold@uiowa.edu>
2025-12-13 19:35:23 -0600

    Partway through adding snapshots

--- app/models/event_recipient.rb
+++ app/models/event_recipient.rb
@@ -4,4 +4,19 @@ class EventRecipient < ApplicationRecord
   has_one :event_recipient_budget, dependent: :destroy
 
   validates :event_id, uniqueness: { scope: :recipient_id }
+
+
+  def [](key)
+    snapshot[key.to_s]
+  end
+
+  def sync_back!(user)
+    raise "No source recipient" unless source_recipient_id
+
+    recipient = Recipient.find(source_recipient_id)
+    raise "Not authorized" unless recipient.user_id == user.id
+
+    recipient.update!(snapshot)
+  end
+
 end
