37c46ffb87cf08e2426273fc2d250c69340de991
Jabe03 <jbergthold@uiowa.edu>
2025-12-14 02:08:36 -0600

    Linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # has_many :friendships, dependent: :destroy
+  # has_many :friends, through: :friendships, source: :friend
 
   has_many :event_users, dependent: :destroy
 
