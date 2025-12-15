8ea48dd1eab29b246676b1cfd59ce3d0206885b6
Nancy Nahra <nnahra@uiowa.edu>
2025-12-14 20:26:21 -0600

    Rspec tests for gift list

--- spec/models/gift_list_spec.rb
+++ spec/models/gift_list_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe GiftList, type: :model do
-  let(:recipient_double) { instance_double(User) }
+  let(:user) do
+    User.create!(
+      name: "Test User",
+      email: "test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+  end
+
+  let(:recipient) do
+    Recipient.create!(
+      name: "Test Recipient",
+      user_id: user.id,
+      gender: :male,
+      relation: "Friend",
+      age: 25
+    )
+  end
+
   describe 'validations and associations' do
     it 'is valid with a name and a recipient' do
-      gift_list = GiftList.new(name: "Birthday List", recipient: recipient_double)
-      expect(gift_list.valid?).to eq(true)
+      gift_list = GiftList.new(name: "Birthday List", recipient: recipient)
+      expect(gift_list).to be_valid
     end
 
     it 'is invalid without a name' do
-      gift_list = GiftList.new(name: nil, recipient: recipient_double)
+      gift_list = GiftList.new(name: nil, recipient: recipient)
       expect(gift_list).not_to be_valid
       expect(gift_list.errors[:name]).to include("can't be blank")
     end
